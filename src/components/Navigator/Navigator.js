c7a7e8d92d7a83c608944f1f9ca63f8700e0c78e
Luiz Carlos B Pereira <carlinhospereira12@hotmail.com>
2021-04-11 22:17:24 -0300

    PRimeiro commit

---
+++ src/components/Navigator/Navigator.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Navigator.css';
+
+function Navigator() {
+    return (
+        <div className="body">
+            <h1> Testess</h1>
+        </div>
+    );
+}
+
+
+export default Navigator;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App/App';
+import Navigator from './components/Navigator/Navigator';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Navigator />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
